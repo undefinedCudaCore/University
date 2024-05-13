a9d1af71450f66a5d01de67aeff699f09aafcfd9
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-05-13 18:29:44 +0300

    Added UniversityContext basics

--- University/Database/UniversityContext.cs
+++ University/Database/UniversityContext.cs
@@ -1,6 +1,24 @@
-﻿namespace University.Database
+﻿using Microsoft.EntityFrameworkCore;
+using University.Models;
+
+namespace University.Database
 {
-    internal class UniversityContext
+    internal class UniversityContext : DbContext
     {
+        public UniversityContext()
+        {
+            ConnectionString = "Data Source=LENOVOY520\\SQLEXPRESS;Initial Catalog=MyFileFolderDB;" +
+                "Integrated Security=True;Encrypt=False";
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public string ConnectionString { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
     }
 }
